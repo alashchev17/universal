573df8ef3e142c54d360b6833465a1029dd48e74
alashchev17 <andrew.lashchev15@gmail.com>
2021-08-04 20:39:44 +0300

    create: responsive stylesheet for main section and header

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
   // табы
   var tabsItem = $(".heading__tabs-item");
   var contentItem = $(".content__item");
+  var mobileButton = $(".mobile-button");
+  var mobileMenu = $(".mobile-menu");
 
   tabsItem.on("click", function (event) {
     var activeContent = $(this).attr("data-target");
@@ -9,5 +11,10 @@ $(document).ready(function () {
     contentItem.removeClass("content__item--active");
     $(activeContent).addClass("content__item--active");
     $(this).addClass("heading__tabs-item--active");
+  });
+
+  mobileButton.on("click", function () {
+    mobileMenu.toggleClass("mobile-menu--active");
+    mobileButton.toggleClass("mobile-button--active");
   });
 });
