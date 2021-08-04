2906a8fc711c59f0e6e20ed48e8d40309096ff43
alashchev17 <andrew.lashchev15@gmail.com>
2021-08-04 19:16:12 +0300

    create: main section with working tabs

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  // табы
+  var tabsItem = $(".heading__tabs-item");
+  var contentItem = $(".content__item");
+
+  tabsItem.on("click", function (event) {
+    var activeContent = $(this).attr("data-target");
+    tabsItem.removeClass("heading__tabs-item--active");
+    contentItem.removeClass("content__item--active");
+    $(activeContent).addClass("content__item--active");
+    $(this).addClass("heading__tabs-item--active");
+  });
+});
